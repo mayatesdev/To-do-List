37433d6f0247482ce83e4a0bd38dcf4924308912
Maya Tesler <mayates.dev@gmail.com>
2021-01-19 08:03:15 +0200

    change the js file

--- script.js
+++ script.js
@@ -4,16 +4,34 @@ const divElist = document.querySelector('#list');
 const valueInpt = [];
 
 btn.addEventListener('click',()=>{
-   valueInpt.push(inputEl.value)
-   const el = createitem(inputEl.value);
-   divElist.appendChild(el)
-   console.log(valueInpt);
+   if(inputEl.value.length > 0){
+      valueInpt.push(inputEl.value)
+      const el = createitem(inputEl.value);
+      divElist.appendChild(el)
+      console.log(valueInpt);
+      inputEl.value = ""
+   }else{
+      alert("eneter your task")
+   }
 })
 
 
 
 function createitem(input){
 const divEl = document.createElement('div');
+divEl.classList.add('itemList');
 divEl.innerHTML = `<p>${input}<p>`;
+
+divEl.addEventListener('click', ()=>{
+   if(divEl.className === 'itemList'){
+      divEl.classList.toggle('itemList');
+      divEl.classList.toggle('itemListRemove');
+   }else{
+     divEl.classList.toggle('itemList');
+      divEl.classList.toggle('itemListRemove'); 
+   }
+})
 return divEl;
 }
+
+
